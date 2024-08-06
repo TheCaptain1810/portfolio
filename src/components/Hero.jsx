d0b2792e55947d409f989e3f57eb0c325a7b1ebc
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-08-06 23:33:55 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className='relative w-full h-screen mx-auto flex flex-col justify-center text-center'>
+    <section className='relative w-full h-screen mx-auto flex flex-col justify-center'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
@@ -43,7 +43,7 @@ const Hero = () => {
       </div>
       
       {!isMobile ? (<ComputersCanvas />) 
-        : <p className={`${styles.sectionSubText} text-white font-bold mt-40`}>Scroll down or click on the icon below<br /> to get to know me better!</p>
+        : <p className={`${styles.sectionSubText} text-white text-center font-bold mt-40`}>Scroll down or click on the icon below<br /> to get to know me better!</p>
       }
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
