2e709d548a32987bd62ea36e18b2c736bb4c9f7f
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-08-06 22:17:52 +0530

    Responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,35 @@
 import { motion } from 'framer-motion';
+import { useState, useEffect } from 'react';
 
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 570px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   return (
-    <section className='relative w-full h-screen mx-auto'>
+    <section className='relative w-full h-screen mx-auto flex flex-col justify-center text-center'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
@@ -19,7 +42,9 @@ const Hero = () => {
         </div>
       </div>
       
-      <ComputersCanvas />
+      {!isMobile ? (<ComputersCanvas />) 
+        : <p className={`${styles.sectionSubText} text-white font-bold mt-40`}>Scroll down or click on the icon below<br /> to get to know me better!</p>
+      }
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
