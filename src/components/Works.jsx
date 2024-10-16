87b23ecaf7b72705471c6a1f8893531ac665e6b7
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-10-16 14:40:44 +0530

    removed some unnecessary animations

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,9 +9,7 @@ import { fadeIn, textVariant } from '../utils/motion';
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-    >
+    <motion.div>
       <Tilt
         options={{
           max: 45,
@@ -63,12 +61,11 @@ const Works = () => {
       </motion.div>
 
       <div className='w-full flex'>
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+        <p
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
-        </motion.p>
+        </p>
       </div>
 
       <div className='mt-20 flex flex-wrap gap-7'>
